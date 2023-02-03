528d5e5428450b362d73314ae93f5542fd3e5d6e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-03 13:37:33 -0500

    :boom: Fix eslint but add prettier problem

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 
 function App(): JSX.Element {
-  const unusedVar = "Check eslint";
-  return <Text>Hello world</Text>;
+        return <Text>Hello world</Text>;
 }
 
 export default App;
