62455ae9a15d98956fe3b6eddafd7256c5171bb4
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-10 20:35:39 -0500

    :sparkles: Nested navigation and some initial pages

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MapView } from '../pages/MapView';
+import { CaptureView } from '../pages/CaptureView';
+import { CombatView } from '../pages/CombatView';
+import { BottomNavigation } from './BottomNavigation';
+
+const Stack = createStackNavigator();
+export const StackNavigation = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName='Application'
+      screenOptions={{ headerShown: false }}
+    >
+      {/* Views that are not included in the bottom tabs navigation but are reachable using the navigator hook */}
+      <Stack.Screen name='Map' component={MapView} />
+      <Stack.Screen name='Capture' component={CaptureView} />
+      <Stack.Screen name='Combat' component={CombatView} />
+      {/* Views that includes the bottom tabs navigation */}
+      <Stack.Screen name='Application' component={BottomNavigation} />
+    </Stack.Navigator>
+  );
+};
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,20 @@
+import { NavigationProp } from '@react-navigation/native';
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
-export const Profile = () => {
-  return <Text>Profile</Text>;
+interface ProfileProps {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  navigation: NavigationProp<any, any>;
+}
+
+export const Profile = ({ navigation }: ProfileProps) => {
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Button
+        title='Go to capture'
+        onPress={() => navigation.navigate('Capture')}
+      />
+    </View>
+  );
 };
