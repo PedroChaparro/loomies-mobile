1e0c873778f6638b0c3b87a4a661fbb10ea176cf
woynert <whayner.porras.2020@upb.edu.co>
2023-02-16 07:52:10 -0500

    🛂 feat(map3d): Get user location

---
+++ src/components/map3d/geolocation.ts
@@ -0,0 +1,50 @@
+import { PermissionsAndroid } from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+
+import { iPosition } from './mapInterfaces';
+
+export const requestLocationPermission = async (): Promise<boolean> => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Access location required',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK'
+      }
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+export const getPosition = async (): Promise<iPosition | null> => {
+  const locationPermission: boolean = await requestLocationPermission();
+  if (!locationPermission) return null;
+
+  Geolocation.getCurrentPosition(
+    (position) => {
+      return {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude
+      };
+    },
+    (error) => {
+      // See error code charts below.
+      console.log(error.code, error.message);
+    },
+    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  );
+
+  return null;
+};
