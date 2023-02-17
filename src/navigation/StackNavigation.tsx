0e6883e07eb8533584237bce9603be6408775c8e
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-02-17 18:32:35 -0500

    :construction: Create login and signup pages / routes

--- src/navigation/StackNavigation.tsx
+++ src/navigation/StackNavigation.tsx
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { MapView } from '../pages/MapView';
 import { CaptureView } from '../pages/CaptureView';
 import { CombatView } from '../pages/CombatView';
+import { Login } from '../pages/Login';
+import { Signup } from '../pages/Signup';
 import { BottomNavigation } from './BottomNavigation';
 
 const Stack = createStackNavigator();
@@ -16,6 +18,8 @@ export const StackNavigation = () => {
       <Stack.Screen name='Map' component={MapView} />
       <Stack.Screen name='Capture' component={CaptureView} />
       <Stack.Screen name='Combat' component={CombatView} />
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Signup' component={Signup} />
       {/* Views that includes the bottom tabs navigation */}
       <Stack.Screen name='Application' component={BottomNavigation} />
     </Stack.Navigator>
