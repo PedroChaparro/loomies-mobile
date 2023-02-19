0fa369ab430b20fbd97d3f29b4e25e451fbc4b98
woynert <whayner.porras.2020@upb.edu.co>
2023-02-19 10:32:24 -0500

    🧑‍💻 feat(map3d): Added debug position controls

--- src/context/UserPositionProvider.tsx
+++ src/context/UserPositionProvider.tsx
@@ -1,24 +1,57 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { Text } from 'react-native';
 import { iPosition, getPosition } from '@src/services/geolocation';
 
-//interface iUserPositionContext {
-  //userPosition: iPosition | null;
-//}
+export interface iUserPositionContext {
+  userPosition: iPosition | null;
+  cachedUserPosition: iPosition | null;
+  debugMovePosition: (_deltaPos: iPosition) => void;
+}
 
-export const UserPositionContext = createContext<iPosition | null>(null);
+const DEBUG = true;
+
+export const UserPositionContext = createContext<iUserPositionContext>({
+  userPosition: null,
+  cachedUserPosition: null,
+  debugMovePosition: (_deltaPos: iPosition) => {
+    return;
+  }
+});
 
 export const UserPositionProvider = (props: { children: any }) => {
+  const [cachedUserPosition, setCachedUserPosition] =
+    useState<iPosition | null>(null);
   const [userPosition, setUserPosition] = useState<iPosition | null>(null);
 
   const updateUserPosition = async () => {
-    console.log("Checking pos");
+    console.log('Checking pos');
     // try to request position
     const newPos = await getPosition();
-    if (newPos) setUserPosition(newPos);
+    if (newPos) {
+      setUserPosition(newPos);
+
+      if (!cachedUserPosition) setCachedUserPosition(newPos);
+    }
 
     // wait 5 seconds
+    if (DEBUG) return;
+
     await delay(5000);
     updateUserPosition();
+  };
+
+  const debugMovePosition = async (deltaPos: iPosition) => {
+    console.log('New position ', userPosition);
+    setUserPosition((pos) => {
+      if (pos) {
+        return {
+          lon: pos.lon + deltaPos.lon,
+          lat: pos.lat + deltaPos.lat
+        };
+      }
+      return pos;
+    });
+    console.log('New position ', userPosition);
   };
 
   useEffect(() => {
@@ -27,7 +60,12 @@ export const UserPositionProvider = (props: { children: any }) => {
   }, []);
 
   return (
-    <UserPositionContext.Provider value={ userPosition }>
+    <UserPositionContext.Provider
+      value={{ userPosition, cachedUserPosition, debugMovePosition }}
+    >
+      <Text>
+        lon {userPosition?.lon} , lat {userPosition?.lat}
+      </Text>
       {props.children}
     </UserPositionContext.Provider>
   );
