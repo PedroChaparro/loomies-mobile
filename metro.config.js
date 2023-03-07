a71b04e0173be802d5b148559063307b964652d2
woynert <whayner.porras.2020@upb.edu.co>
2023-03-07 11:49:51 -0500

    fix: Include only needed file formats in metro config

--- metro.config.js
+++ metro.config.js
@@ -16,34 +16,13 @@ module.exports = {
   },
   resolver: {
     assetExts: [
-      'bmp', // Image formats
-      'gif',
       'jpg',
       'jpeg',
       'png',
-      'psd',
       'svg',
-      'webp', // Video formats
-      'm4v',
-      'mov',
-      'mp4',
-      'mpeg',
-      'mpg',
-      'webm', // Audio formats
-      'aac',
-      'aiff',
-      'caf',
-      'm4a',
-      'mp3',
-      'wav', // Document formats
-      'html',
-      'pdf',
-      'yaml',
-      'yml', // Font formats
-      'otf',
-      'ttf', // Archives (virtual files)
-      'zip',
-      'glb' // 3D
+      'wav',
+      'ttf',
+      'glb'
     ]
   }
 };
