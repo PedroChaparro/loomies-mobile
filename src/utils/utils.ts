c08891e1278bab72874a257d18dcaee4ea98fa7c
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-03-20 15:20:54 -0500

    feat(loomies): 🍱 Require loomies images on load

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { ImageSourcePropType } from 'react-native';
+
 const colors: {
   [key: string]: string;
 } = {
@@ -11,6 +13,29 @@ const colors: {
   ELECTRIC: '#FFE783',
   ROCK: '#DFDFDF',
   IRON: '#D7D7ED'
+};
+
+export const images: {
+  [key: string]: ImageSourcePropType;
+} = {
+  '001': require('@assets/images/loomies/001.png'),
+  '002': require('@assets/images/loomies/002.png'),
+  '003': require('@assets/images/loomies/003.png'),
+  '004': require('@assets/images/loomies/004.png'),
+  '005': require('@assets/images/loomies/005.png'),
+  '006': require('@assets/images/loomies/006.png'),
+  '007': require('@assets/images/loomies/007.png'),
+  '008': require('@assets/images/loomies/008.png'),
+  '009': require('@assets/images/loomies/009.png'),
+  '010': require('@assets/images/loomies/010.png'),
+  '011': require('@assets/images/loomies/011.png'),
+  '012': require('@assets/images/loomies/012.png'),
+  '013': require('@assets/images/loomies/013.png'),
+  '014': require('@assets/images/loomies/014.png'),
+  '015': require('@assets/images/loomies/015.png'),
+  '016': require('@assets/images/loomies/016.png'),
+  '017': require('@assets/images/loomies/017.png'),
+  '018': require('@assets/images/loomies/018.png')
 };
 
 /**
