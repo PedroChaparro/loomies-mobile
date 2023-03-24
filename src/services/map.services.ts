520af31c5a4a6b00a4bddb22001772c211829243
woynert <whayner.porras.2020@upb.edu.co>
2023-03-24 11:51:38 -0500

    feat(MapLoomies): Near wild loomies request

--- src/services/map.services.ts
+++ src/services/map.services.ts
@@ -1,14 +1,15 @@
 import Axios from 'axios';
 
-import { iRequestGym, iRequestNearGyms } from '@src/types/requestInterfaces';
+import { iRequestGym, iRequestNearGyms, iRequestNearLoomies } from '@src/types/requestInterfaces';
 import { iGym, iPosition } from '@src/types/mapInterfaces';
 import { getStorageData } from './storage.services';
 import { CONFIG } from './config.services';
+import { TWildLoomies } from '@src/types/types';
 const { API_URL } = CONFIG;
 
 // Returns an array of iGyms
 
-export const getNearGyms = async (
+export const requestNearGyms = async (
   userPos: iPosition
 ): Promise<iGym[] | null> => {
   try {
@@ -53,6 +54,47 @@ export const getNearGyms = async (
     });
 
     return gyms;
+  } catch (error) {
+    return null;
+  }
+};
+
+// Returns an array of iGyms
+
+export const requestWildLoomies = async (
+  userPos: iPosition
+): Promise<TWildLoomies[] | null> => {
+  try {
+    // get access token from the storage
+    const [accessToken, error] = await getStorageData('accessToken');
+    if (error || !accessToken) {
+      return null;
+    }
+
+    // make request
+    const response = await Axios.post(
+      `${API_URL}/near_loomies`,
+      {
+        latitude: userPos.lat,
+        longitude: userPos.lon
+      },
+      {
+        headers: {
+          'Access-Token': accessToken
+        }
+      }
+    );
+
+    // cast check
+    const rawData: object = response.data;
+    if ((rawData as iRequestNearLoomies) === undefined) {
+      return null;
+    }
+
+    const data: iRequestNearLoomies = rawData as iRequestNearLoomies;
+
+    // return wild loomies
+    return data.loomies;
   } catch (error) {
     return null;
   }
