0de7a495b56ec9238faa1da7ecb6614e980510f0
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-03-28 00:07:02 -0500

    :recycle: refactor: Making LinkCard as reusable component
    TouchableWithoutFeedback and components become in a reusable component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import FeatherIcon from 'react-native-vector-icons/Feather';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationProp } from '@react-navigation/core';
 import { CustomButton } from '../components/CustomButton';
+import { LinkCard } from '../components/LinkCard';
 
 interface ProfileProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -25,17 +25,11 @@ export const Profile = ({ navigation }: ProfileProps) => {
         <Text style={[Styles.mainTitle, Styles.mainTitleBold]}>
           {user?.username}
         </Text>
-        <Text style={Styles.mainTitle}>{user?.email}</Text>
-        <TouchableOpacity
-          style={Styles.linkCard}
-          onPress={redirectToResetPassword}
-        >
-          <View style={Styles.groupIconText}>
-            <FeatherIcon name={'lock'} size={20} color={'black'} />
-            <Text style={Styles.linkText}>Reset Password</Text>
-          </View>
-          <FeatherIcon name={'chevron-right'} size={20} color={'black'} />
-        </TouchableOpacity>
+        <LinkCard
+          iconName='lock'
+          displayText='Reset Password'
+          callback={redirectToResetPassword}
+        />
         <View style={Styles.form}>
           {/* todo */}
           <CustomButton
@@ -62,8 +56,8 @@ const Styles = StyleSheet.create({
     color: '#fff',
     fontSize: 28,
     fontWeight: 'bold',
-    marginTop: 100,
-    marginBottom: 10,
+    justifyContent: 'center',
+    padding: 50,
     textAlign: 'center',
     textTransform: 'uppercase'
   },
@@ -80,27 +74,6 @@ const Styles = StyleSheet.create({
   },
   mainTitleBold: {
     fontWeight: 'bold'
-  },
-  linkCard: {
-    alignSelf: 'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 16,
-    padding: 4,
-    paddingLeft: 8,
-    width: '80%',
-    backgroundColor: '#f2f1ed',
-    justifyContent: 'space-between',
-    height: 42,
-    borderRadius: 4
-  },
-  groupIconText: {
-    alignSelf: 'center',
-    flexDirection: 'row'
-  },
-  linkText: {
-    color: '#8f8f8f',
-    marginLeft: 12
   },
   form: {
     alignSelf: 'center',
