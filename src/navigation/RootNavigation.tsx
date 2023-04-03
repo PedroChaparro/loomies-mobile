4e08fb3dd8238aa32889decc50fb57eed20b13bf
woynert <woynertgamer@gmail.com>
2023-04-03 13:38:46 -0500

    feat(Navigation): Add navigationRef
    - Allows navigating from everywhere

---
+++ src/navigation/RootNavigation.tsx
@@ -0,0 +1,10 @@
+import { createNavigationContainerRef } from '@react-navigation/native';
+
+export const navigationRef = createNavigationContainerRef()
+
+export function navigate(name: string, params: any) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name as never, params as never);
+  }
+}
+
