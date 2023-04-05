0921c487177fababafcec50149f1c50ed4d72bf6
woynert <whayner.porras.2020@upb.edu.co>
2023-04-05 09:53:31 -0500

    feat(LoomieDetails): Stop babylonjs engine on unfocus

--- src/pages/LoomieDetails.tsx
+++ src/pages/LoomieDetails.tsx
@@ -1,13 +1,12 @@
-import { RouteProp } from '@react-navigation/core';
+import { RouteProp, useFocusEffect } from '@react-navigation/core';
 import { TCaughtLoomies } from '@src/types/types';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import { colors } from '@src/utils/utils';
 import { LoomieLevelBar } from '@src/components/LoomieDetails/LoomieLevelBar';
 import { LoomieStatsTable } from '@src/components/LoomieDetails/LoomieStatsTable';
 import { LoomieTypes } from '@src/components/LoomieDetails/LoomieTypes';
-import { APP_SCENE, BabylonContext } from '@src/context/BabylonProvider';
-import { EngineView } from '@babylonjs/react-native';
+import { BabylonContext } from '@src/context/BabylonProvider';
 import { Loomie3DModelPreview } from '@src/components/LoomieDetails/Loomie3DModelPreview';
 
 interface IProps {
@@ -17,16 +16,21 @@ interface IProps {
 export const LoomieDetails = ({ route }: IProps) => {
 
   const [loomie, setLoomie] = useState<TCaughtLoomies | null>(null);
-  const { showSceneDetails } = useContext(BabylonContext);
+  const { showSceneDetails, showSceneNone } = useContext(BabylonContext);
 
   useEffect(() => {
     // Try to get the loomie from the route params
     const loomieFromRoute = route?.params?.loomie;
     if (loomieFromRoute) setLoomie(loomieFromRoute);
-
-    // Toggle rendered scene in engine
-    showSceneDetails();
   }, []);
+
+  // toggle render loop on focus events
+  useFocusEffect(
+    React.useCallback(() => {
+      showSceneDetails();
+      return () => showSceneNone();
+    }, [])
+  );
 
   if (!loomie) return null;
   const mainColor = loomie.types[0].toUpperCase();
