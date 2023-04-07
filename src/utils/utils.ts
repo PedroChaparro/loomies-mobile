d6c51e79c0235675d83192c595c56bd158232d13
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-04-07 17:06:27 -0500

    fix: 🐛 Parse Loomie experience env variables to number

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -54,7 +54,8 @@ export const images: {
 
 export function getRequiredExperienceFromLevel(level: number): number {
   return (
-    Math.log10(level) * GAME_EXPERIENCE_FACTOR + GAME_MIN_REQUIRED_EXPERIENCE
+    Math.log10(level) * Number(GAME_EXPERIENCE_FACTOR) +
+    Number(GAME_MIN_REQUIRED_EXPERIENCE)
   );
 }
 
