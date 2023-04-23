4c14f306c24c344ff1ec335da42d34cf142a1c15
woynert <whayner.porras.2020@upb.edu.co>
2023-04-23 12:09:31 -0500

    fix(config): Put together API urls

--- src/services/config.services.ts
+++ src/services/config.services.ts
@@ -1,7 +1,14 @@
 import Config from 'react-native-config';
 
+const TSL = Config.TSL == 'true';
+
 export const CONFIG = {
-  API_URL: Config.API_URL,
+  API_URL: TSL
+    ? `https://${Config.API_URL}:${Config.PORT}`
+    : `http://${Config.API_URL}:${Config.PORT}`,
+  WS_URL: TSL
+    ? `wss://${Config.API_URL}:${Config.PORT}`
+    : `ws://${Config.API_URL}:${Config.PORT}`,
   MAP_DEBUG: Config.MAP_DEBUG == 'true',
   GAME_MIN_REQUIRED_EXPERIENCE: Config.GAME_MIN_REQUIRED_EXPERIENCE,
   GAME_EXPERIENCE_FACTOR: Config.GAME_EXPERIENCE_FACTOR,
